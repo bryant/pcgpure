33ea8389f1e2df0b48b60faceecc11118e416fb9
bryant <bryant@defrag.in>
2015-10-03 17:18:21 -0400

    cleanup

--- src/PcgPure.hs
+++ src/PcgPure.hs
@@ -2,37 +2,10 @@
 
 module PcgPure where
 
---import System.IO (withFile, IOMode(ReadMode), Handle)
 import qualified Data.Vector.Unboxed.Mutable as UM
-import Data.IORef (IORef, readIORef, modifyIORef')
 import Data.Word (Word32, Word64)
 import Data.Bits (finiteBitSize, FiniteBits, xor, unsafeShiftR, rotateR, (.|.))
-import qualified Data.ByteString as B
-import Control.Monad.State (get, put, State, runState)
-import System.Random
-
-{-
-class Pcgable st where
-    width_state :: Int
-    advance :: st -> st
-    pcg_self_init :: IO (Pcg64State st)
-    pcg_init :: st -> Pcg64State st
-
-newtype Pcg64State st = Pcg64State st
--}
-
-data Pcg64State
-    = Pcg64State
-        {-# UNPACK #-} !Word64  -- state
-        {-# UNPACK #-} !Word64  -- inc
-
-type Pcg64 = State Pcg64State
-
-pcg32_self_init :: IO Pcg64State
-pcg32_self_init = do
-    initial <- randomIO
-    increment <- randomIO
-    return $ Pcg64State initial increment
+import System.Random (randomIO)
 
 pcg32_self_init_vec :: IO (UM.IOVector Word64)
 pcg32_self_init_vec = do
@@ -48,35 +21,9 @@ pcg32_init state increment = do
     UM.unsafeWrite v 1 increment
     return v
 
-{-
-pcg32_init = withFile "/dev/urandom" ReadMode $ \h -> do
-    st <- B.hGetSome h 8
-    -- multiplier <- B.hGetSome h 8
-    let multiplier = 6364136223846793005
-    increment <- B.hGetSome h 8
-    return . Pcg64State st $ \st -> st * multiplier + increment
-
-get_some :: FiniteBits a => Handle -> a -> IO a
-get_some handle numty = foldl' g
-    B.unpack B.hGetSome handle width
--}
-
-pcg32_advance :: Pcg64State -> Pcg64State
-pcg32_advance (Pcg64State st inc) = Pcg64State st inc
-    where
-    multiplier = 6364136223846793005
-    st' = multiplier * st + inc
-
 pcg32_advance_vec :: Word64 -> Word64 -> Word64
 pcg32_advance_vec st inc = multiplier * st + inc
     where multiplier = 6364136223846793005
-
-pcg32_int32 :: Pcg64 Word32
-pcg32_int32 = do
-    st <- get
-    let !st'@(Pcg64State state _) = {- pcg32_advance -} st
-    --put st'
-    return $ fromIntegral {- permute -}state
 
 pcg32_int32_io :: (Word64 -> Word32) -> UM.IOVector Word64 -> IO Word32
 pcg32_int32_io adv st = do
@@ -102,12 +49,6 @@ xsh_rr n =
         xsh_32 = u32_from_bit (64 - 32 - 5) xsh
     in xsh_32 `rotateR` top_bits xsh 5
     where shift = (64 - 32 + 5) `quot` 2
-{-
-    where
-    !rv = xored `rotateR` rot
-    !xored = xorshift n ((64 - 32 + 5) `quot` 2)
-    !rot = fromIntegral (top_bits n 5)
--}
 
 xorshift :: FiniteBits a => a -> Int -> a
 xorshift n steps = n `xor` (n `unsafeShiftR` steps)
