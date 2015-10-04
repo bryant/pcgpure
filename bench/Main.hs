ba25cadbd105d57f72c17bca4a742984a986fbf0
bryant <bryant@defrag.in>
2015-10-03 22:31:34 -0400

    add the c wrapper to benchmarks

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BangPatterns, FlexibleContexts #-}
 
 module BenchMain where
 
@@ -8,14 +8,16 @@ import Criterion.Main (defaultMain, bgroup, bench, nfIO, nf)
 import Control.Monad.State (runState)
 import PcgPure
 import qualified System.Random.MWC as MWC
+import qualified System.Random.PCG as PCG
 import qualified Data.Vector.Unboxed as Unboxed
 
 main = do
     mwcgen <- MWC.createSystemRandom
     pcggen <- pcg32_self_init_vec
-    bench_main mwcgen pcggen
+    ffigen <- PCG.createSystemRandom
+    bench_main mwcgen pcggen ffigen
 
-bench_main mwc pcg = defaultMain
+bench_main mwc pcg ffipcg = defaultMain
     [ bgroup (show veclen) [
         bench "pcgpure.rxs_m_xs" . nfIO $ do
             Unboxed.replicateM veclen $ pcg32_int32_io rxs_m_xs pcg
@@ -23,5 +25,7 @@ bench_main mwc pcg = defaultMain
             Unboxed.replicateM veclen $ pcg32_int32_io xsh_rr pcg
         , bench "mwc-random" . nfIO $ do
             MWC.uniformVector mwc veclen :: IO (Unboxed.Vector Word32)
+        , bench "pcg-random" . nfIO $ do
+            Unboxed.replicateM veclen $ PCG.uniform ffipcg :: IO (Unboxed.Vector Word32)
         ]
     | veclen <- map (10 ^) [3..4]]
