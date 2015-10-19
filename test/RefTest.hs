538a57d83ca0c37d2104d2d467bd479b261218ce
bryant <bryant@defrag.in>
2015-10-19 09:34:20 -0400

    test all variants

--- test/RefTest.hs
+++ test/RefTest.hs
@@ -1,10 +1,12 @@
 module RefTest where
 
+import qualified PcgPure as P
+
 import Data.Word (Word32, Word64)
-import Foreign.ForeignPtr (withForeignPtr, newForeignPtr)
+import Foreign.ForeignPtr (ForeignPtr, withForeignPtr, newForeignPtr)
 import Foreign.Ptr (FunPtr, Ptr)
-import PcgPure (pcg32_init, pcg32_int32, rxs_m_xs, xsh_rr)
-import Control.Monad (void)
+import Control.Monad (void, when)
+import Data.IORef (readIORef, writeIORef, newIORef)
 
 data XshRr
 
@@ -12,40 +14,66 @@ foreign import ccall "init_xsh_rr" init_xsh_rr :: Word64 -> IO (Ptr XshRr)
 foreign import ccall "&free_xsh_rr" free_xsh_rr :: FunPtr (Ptr XshRr -> IO ())
 foreign import ccall "next_xsh_rr" next_xsh_rr :: Ptr XshRr -> IO Word32
 
-new_xsh_rr seed = init_xsh_rr seed >>= newForeignPtr free_xsh_rr
-
 data RxsMXs
 
 foreign import ccall "init_rxs_m_xs" init_rxs_m_xs :: Word64 -> IO (Ptr RxsMXs)
 foreign import ccall "&free_rxs_m_xs" free_rxs_m_xs :: FunPtr (Ptr RxsMXs -> IO ())
 foreign import ccall "next_rxs_m_xs" next_rxs_m_xs :: Ptr RxsMXs -> IO Word32
 
-new_rxs_m_xs seed = init_rxs_m_xs seed >>= newForeignPtr free_rxs_m_xs
+data Variant ffiptr
+    = Variant
+    { ref_init :: Word64 -> IO (ForeignPtr ffiptr)
+    , ref_next :: ForeignPtr ffiptr -> IO Word32
+    , permuter :: Word64 -> Word32
+    }
+
+rxs_m_xs :: Variant RxsMXs
+rxs_m_xs = Variant
+    { ref_init = \seed -> init_rxs_m_xs seed >>= newForeignPtr free_rxs_m_xs
+    , ref_next = \fptr -> withForeignPtr fptr next_rxs_m_xs
+    , permuter = P.rxs_m_xs
+    }
+
+xsh_rr :: Variant XshRr
+xsh_rr = Variant
+    { ref_init = \seed -> init_xsh_rr seed >>= newForeignPtr free_xsh_rr
+    , ref_next = \fptr -> withForeignPtr fptr next_xsh_rr
+    , permuter = P.xsh_rr
+    }
 
 default_increment :: Word64
 default_increment = 1442695040888963407
 
-compare_gens :: Int -> IO Word32 -> IO Word32 -> IO [(Word32, Word32)]
-compare_gens rounds gen gen' =
-    zip <$> sequence (replicate rounds gen) <*> sequence (replicate rounds gen')
+test_variant :: Int -> Variant k -> Word64 -> IO (IO (Maybe (Word32, Word32)))
+test_variant rounds variant seed = do
+    refgen <- ref_init variant seed
+    pcgnext <- mk_pcg (permuter variant) seed
+    remaining <- newIORef rounds  -- what a hack
+    let gen = do
+        n <- readIORef remaining
+        if n <= 0 then return Nothing else do
+            writeIORef remaining $ n - 1
+            Just <$> ((,) <$> ref_next variant refgen <*> pcgnext)
+    return gen
 
 mk_pcg permute state = do
     let st = state + default_increment  -- from pcg_random.hpp
-    gen <- pcg32_init st default_increment
-    let next = pcg32_int32 permute gen
+    gen <- P.pcg32_init st default_increment
+    let next = P.pcg32_int32 permute gen
     void next  -- "state := bump(state + increment)" from pcg_random.hpp
     return next
 
+main :: IO ()
 main = do
-    pcgpure <- mk_pcg rxs_m_xs 23
-    reference <- new_rxs_m_xs 23
-
-    outputs <- compare_gens 1024 pcgpure (withForeignPtr reference next_rxs_m_xs)
-
-    putStrLn "rxs_m_xs (actual, expected):"
-    sequence_ [log actual expected | (actual, expected) <- outputs]
+    runner <- test_variant 256 rxs_m_xs 23
+    loop runner verbosity
+    runner <- test_variant 256 xsh_rr 23
+    loop runner verbosity
     where
-    log a e
-        | a == e = putStrLn $ "✓ " ++ case_
-        | otherwise = putStrLn $ "✗ " ++ case_
-        where case_ = show a ++ "\t" ++ show e
+    verbosity = True
+    loop r v = r >>= \res -> case res of
+        Nothing -> return ()
+        Just (expected, a)
+            | expected == a -> when v (putStrLn $ "✓ " ++ case_) >> loop r v
+            | otherwise -> putStrLn $ "✗ " ++ case_
+            where case_ = show expected ++ "\t" ++ show a
